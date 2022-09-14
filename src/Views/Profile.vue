1bacdd95a4bf4c26e11f71a13c2ae64ca60ca6f8
sebrs3018 <sebastian.r.s3107@hotmail.com>
2022-09-14 14:42:48 +0200

    with no lazy loading

--- src/Views/Profile.vue
+++ src/Views/Profile.vue
@@ -9,7 +9,6 @@
             <template #observable>
               <!-- <base-box v-if="isVisible" /> -->
               <img
-                v-if="isVisible"
                 src="https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
               />
             </template>
